6573adc6caadf80ab7b4643c3ec16bd50374e7eb
KhashayarS <39525313+KhashayarS@users.noreply.github.com>
2025-04-06 14:23:44 +0330

    Create display library handler - display all the books of the myLibrary array on the page

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,36 @@
-// Define variables
+// Define custom variables
+const myLibrary = [];
+
+// Define document selector variables
+const libraryContainer = document.querySelector("#library-container");
 const addBookBtn = document.querySelector("#add-book-btn");
 const addBookDialog = document.querySelector("#add-book-dialog");
 
+// HTML templates
+const bookTemplateHTML = `
+                <div class="book-container">
+                    <div class="book-container-head">
+                        <h3 class="title"></h3>
+                    </div>
+                    <div class="book-container-body">
+                        <p class="author-text"><strong>Author(s): </strong></p>
+                        <p class="pages-text"><strong>Pages: </strong></p>
+                        <p class="read-text"><strong>Status: </strong><span class="reading-status finished"></span></p>
+                    </div>
+                    <div class="book-container-footer">
+                        <button class="edit-book-btn">Edit</button>
+                    </div>
+                </div>
+`;
 
+// Book cunstructor
 function Book(title, author, pages, read) {
+    const randomID = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = randomID;
     this.info = function () {
         const readString = this.read ? 'read' : 'not read yet';
         const book_info = `${this.title} by ${this.author}, ${this.pages} pages, ${readString}`;
@@ -15,15 +38,51 @@ function Book(title, author, pages, read) {
     }
 }
 
-// Instantiate a book
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tokien', 295, false);
-console.log(theHobbit.info());
+// Instantiate default books and display them on the page
+const bookOne = new Book('The Pragmatic Programmer', 'Andy Hunt, Dave Thomas', 320, true);
+const bookTwo = new Book('Eloquent JavaScript', 'Marijn Haverbeke', 448, false);
+const bookThree = new Book('Clean Code', 'Robert Cecil Martin', 464, true);
+
+myLibrary.push(bookOne, bookTwo, bookThree);
+
+// console.log(bookOne.info());
+// console.log(bookTwo.info());
+// console.log(bookThree.info());
 
 // Display and handle the add book dialog
-addBookBtn.addEventListener("click", addBookDialogHandler);
+addBookBtn.addEventListener("click", openAddBookDialogHandler);
 
 
 // Event Handlers
-function addBookDialogHandler(event) {
+function displayBooksHandler(event) {
+    let bookHTML;
+    myLibrary.forEach(book => {
+        const bookIsRead = book.read ? 'Reading Finished!' : 'Still Reading ...';
+        const bookIsReadClass = book.read ? 'finished' : '';
+        bookHTML = bookTemplateHTML;
+
+        bookHTML = bookHTML.replace(`<h3 class="title"></h3>`, `<h3 class="title">${book.title}</h3>`);
+
+        bookHTML = bookHTML.replace(
+            `<p class="author-text"><strong>Author(s): </strong></p>`,
+            `<p class="author-text"><strong>Author(s): </strong>${book.author}</p>`
+        );
+
+        bookHTML = bookHTML.replace(
+            `<p class="pages-text"><strong>Pages: </strong></p>`,
+            `<p class="pages-text"><strong>Pages: </strong>${book.pages}</p>`
+        );
+
+        bookHTML = bookHTML.replace(
+            `<p class="read-text"><strong>Status: </strong><span class="reading-status finished"></span></p>`,
+            `<p class="read-text"><strong>Status: </strong><span class="reading-status ${bookIsReadClass}">${bookIsRead}</span></p>`
+        );
+
+        libraryContainer.insertAdjacentHTML('beforeend', bookHTML);
+
+    });
+}
+
+function openAddBookDialogHandler(event) {
     addBookDialog.showModal();
 }
